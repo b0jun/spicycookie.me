d48b6d8ac47bd8918c0d268983184e12a581c680
billowycloud <bjkim0228@naver.com>
2020-07-10 11:07:15 +0900

    init setting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React from 'react';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const IndexPage = () => {
+  return <div>I'm Jun</div>;
+};
+
+export default IndexPage;
