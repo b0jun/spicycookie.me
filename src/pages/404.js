0333b8a8f364c8b1e2617c64ced33bcb4612e200
billowycloud <bjkim0228@naver.com>
2020-08-03 23:03:59 +0900

    change 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Layout from '../components/Common/Layout';
+import styled from 'styled-components';
+
+const Block = styled.div`
+  font-family: 'Bangers', cursive;
+  font-size: 6rem;
+`;
 
 const NotFoundPage = () => {
-  return <div>404Page</div>;
+  return (
+    <Layout>
+      <Block>Oops! File Not Found404</Block>
+      The requested URL /asddsadsaas was not found on this server
+    </Layout>
+  );
 };
 
 export default NotFoundPage;
