6c6cd5ee187058b06361f0720b29e12b90ad7283
billowycloud <bjkim0228@naver.com>
2020-08-09 20:15:27 +0900

    install local-storage-callback

--- src/lib/hooks/useLocalStorage.js
+++ src/lib/hooks/useLocalStorage.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
+import storage from 'local-storage-fallback';
 
 export const useLocalStorage = (key, initialValue) => {
-  const $window = typeof window !== `undefined` ? window : {};
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
     try {
       // Get from local storage by key
-      const item = $window.localStorage.getItem(key);
+      const item = storage.getItem(key);
       // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
@@ -26,7 +26,7 @@ export const useLocalStorage = (key, initialValue) => {
       // Save state
       setStoredValue(valueToStore);
       // Save to local storage
-      $window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      storage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.log(error);
